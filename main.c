f273dd12925210d068ffea92f85498cba6f9dedd
alinamihut <alinamihut@student.utcluj.ro>
2019-11-14 23:36:21 +0200

    commit 1

--- main.c
+++ main.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 #define MAX_FOOD_TYPE_NAME 10
-#define MAX_SPECIFIC_FOOD_NAME 20
+#define MAX_SPECIFIC_FOOD_NAME 30
 #define MAX_LINE 10
+#define MAX_DRINK_NAME 10
 void readSpecificFoods(char * specificFoods);
+void readDrinks ( char * drinks);
 
 int main() {
-    //foodtype=brand
-    //max brand name= max food type name
-    //models=specific foods
-    // function for reading drinks
     int noOfFoodTypes;
+    int noOfDrinks;
     printf("Please input number of food types \n");
     scanf("%d", &noOfFoodTypes);
     getchar();
@@ -23,11 +22,12 @@ int main() {
         foodTypes[i] = (char *) malloc(MAX_FOOD_TYPE_NAME * sizeof(char));
         gets(foodTypes[i]);
     }
-    //todo: delete later
+    /*//todo: delete later
     for (int i = 0; i < noOfFoodTypes; i++) {
         printf("Food Type %s\n", foodTypes[i]);
     }
-    int * noOfSpecificFoods = (int*)malloc(noOfFoodTypes* sizeof(int));
+     */
+    int * noOfSpecificFoods = (int*)malloc(noOfFoodTypes * sizeof(int));
     char *** specificFoods = (char***)malloc(noOfFoodTypes * sizeof(char**));
     double ** pricesOfFoods = (double**)malloc(noOfFoodTypes* sizeof(double*));
     for(int i=0; i<noOfFoodTypes; i++) {
@@ -48,41 +48,77 @@ int main() {
             getchar();
         }
     }
-    //todo: delete later
+    /*//todo: delete later
     for (int i = 0; i < noOfFoodTypes; i++)
         for(int j=0;j<noOfSpecificFoods[i];j++)
-            {
-        printf("SpecificFoods and prices %s %lf\n", specificFoods[i][j], pricesOfFoods[i][j]);
+        {
+            printf("Specific Foods and prices %s %lf\n", specificFoods[i][j], pricesOfFoods[i][j]);
+        }
+        */
+
+    printf ("Please input no of drinks %d\n", noOfDrinks);
+    scanf("%d", &noOfDrinks);
+    getchar();
+    char ** drinks;
+    drinks = (char **) malloc(noOfDrinks *  sizeof(char *));
+    double ** pricesOfDrinks = (double**)malloc(noOfDrinks * sizeof(double*));
+    printf ("Please input the drinks: each line in the format <drink> (price): \n");
+    for (int i=0;i<noOfDrinks;i++)
+    {
+        drinks[i] = (char *) malloc(MAX_DRINK_NAME * sizeof(char));
+        pricesOfDrinks[i] = (double*)malloc(noOfDrinks * sizeof(double));
+        readDrinks(drinks[i]);
+        char line[MAX_LINE];
+        gets(line);
+        sscanf(line, "%lf", &pricesOfDrinks[i]);
+
+
     }
-/*
-    // display data
-    printf("The data is:\n");
-    for(int i=0;i<noOfBrands;i++) {
-        // display brand
-        printf("%s: ", brands[i]);
-        for(int j=0;j<noOfModels[i];j++) {
-            printf("(%s, %.2lf) ",models[i][j],prices[i][j]);
+    /*//todo: delete later
+    for (int i = 0; i < noOfDrinks; i++)
+            {
+        printf("Drinks and prices %s %.2lf\n", drinks[i], pricesOfDrinks[i]);
+    }*/
+
+    printf("The food data is:\n");
+    for(int i=0;i<noOfFoodTypes;i++) {
+        printf("%s: ", foodTypes[i]);
+        for(int j=0;j<noOfSpecificFoods[i];j++) {
+            printf("(%s - %.2lf) ",specificFoods[i][j],pricesOfFoods[i][j]);
         }
         printf("\n");
     }
+    printf ("The drink data is: \n");
+    for(int i=0;i<noOfDrinks;i++) {
+        printf("drinks: %s", drinks[i]);
+        if (i<noOfDrinks-1) printf(", ");
+        printf("prices: %s", pricesOfDrinks[i]);
+        if (i<noOfDrinks-1) printf(", ");
 
-    // free memory
-    for(int i=0;i<noOfBrands;i++) {
-        for(int j=0;j<noOfModels;j++) {
-            free(models[i][j]);
-        }
-        free(models[i]);
-        free(prices[i]);
-        free(brands[i]);
+        printf("\n");
     }
-    free(models);
-    free(prices);
-    free(brands);
-    free(noOfModels);
+    for(int i=0;i<noOfFoodTypes;i++) {
+        for(int j=0;j<noOfSpecificFoods;j++) {
+            free(specificFoods[i][j]);
+        }
+        free(specificFoods[i]);
+        free(pricesOfFoods[i]);
+        free(foodTypes[i]);
+    }
+    for(int i=0;i<noOfDrinks;i++) {
+        free(pricesOfDrinks[i]);
+        free(drinks[i]);
+
+    }
+    free(specificFoods);
+    free(pricesOfFoods);
+    free(foodTypes);
+    free (drinks);
+    free (pricesOfDrinks);
 
     return 0;
 }
-  */}
+
 void readSpecificFoods(char * specificFoods) {
     char c = getchar();
     int i=0;
@@ -91,8 +127,18 @@ void readSpecificFoods(char * specificFoods) {
         c = getchar();
         i++;
     }
-   specificFoods[i] = '\0';
+    specificFoods[i] = '\0';
 }
 
 
+void readDrinks ( char * drinks){
+    char c = getchar();
+    int i=0;
+    while(c!='(') {
+        drinks[i] = c;
+        c = getchar();
+        i++;
+    }
+    drinks[i] = '\0';
+}
 
